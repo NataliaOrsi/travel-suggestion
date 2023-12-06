d63caf0a83ff70bd28fd21be2b3dd3ac26f4a8d4
Natalia Orsi <naty.m.orsi@gmail.com>
2023-12-06 14:33:48 +0100

    Added the AI logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+function displayCity(response) {
+  new Typewriter("#city", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
 
+function generateTravelDestination(event) {
+  event.preventDefault();
+
+  let apiKey = "5b303bcbffcb9458t34a63o545f3d664";
+  let instructions = document.querySelector("#user-instructions");
+  let prompt = `User instructions: Generate a travel destination based on ${instructions.value}`;
+  let context =
+    "You're a travel expert and know every city in the World. Your mission is to generate a city destinations in basic HTML. Make sure to follow the user instructions. ONLY provide the name of the city. Do NOT include travel destination";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.classList.remove("hidden");
+  cityElement.innerHTML = `<div class="generating">⏳ Generating a travel destination with the following characteristic ${instructions.value}</div>`;
+
+  axios.get(apiUrl).then(displayCity);
+}
+
+let formElement = document.querySelector("#destination-generator-form");
+
+formElement.addEventListener("submit", generateTravelDestination);
